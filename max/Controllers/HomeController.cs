243b054bcf5d49cb231482dae64cc1816404dab9
Ali Can ALTUNEL <Ali Can ALTUNEL@DESKTOP-EO9U0AB>
2022-05-10 19:21:59 +0300

    Add project files.

---
+++ max/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace max.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Title = "MAX || AnaSayfa";
+            return View();
+        }
+        public ActionResult Slider()
+        {
+            return View();
+        }
+        public ActionResult info()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Projects()
+        {
+            return View();
+        }  
+        public ActionResult Counter()
+        {
+            return View();
+        }   
+        public ActionResult Block()
+        {
+            return View();
+        }
+    }
+}
---
+++ max/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="max.MvcApplication" Language="C#" %>
